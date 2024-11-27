f0389a02111f662ebaadd2c31d72e623dc06c9d8
izraelzz <izraelpicoli@gmail.com>
2024-11-27 20:01:02 -0300

    Commit 5

--- Customer.java
+++ Customer.java
@@ -25,9 +25,6 @@ public class Customer {
       while (rentals.hasMoreElements()) {
          Rental each = (Rental) rentals.nextElement();
    
-         // determine amounts for each line
-         double thisAmount = each.getCharge();
-   
          // add frequent renter points
          frequentRenterPoints++;
          if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
@@ -35,8 +32,8 @@ public class Customer {
    
          // show figures for this rental
          result += "\t" + each.getMovie().getTitle() + "\t" +
-            String.valueOf(thisAmount) + "\n";
-         totalAmount += thisAmount;
+            String.valueOf(each.getCharge()) + "\n";
+         totalAmount += each.getCharge();
       }
    
       // add footer lines
@@ -45,6 +42,7 @@ public class Customer {
          " frequent renter points";
       return result;
    }
+   
    
    private double amountFor(Rental aRental) {
       double thisAmount = 0;
